a8514c10bf3ff65388caeb51669f3fd885c5c581
Luiz Souza <luixsouza12@outlook.com>
2020-11-10 17:16:28 -0300

    create aula02
    Introduction of HTML and PHP

---
+++ Desenvolvimento de Sistema Web II/aula02tarde/index.php
@@ -0,0 +1,13 @@
+<?php
+      /* Variavel: espaço alocado na memória
+      que sofre modificações ao longo da execução do
+      sistema*/
+    $nome = "Luiz";
+    $sobrenome = "Souza";
+    $idade = 17;
+
+    echo '<h1>Este é um exemplo de impressões de variáveis</h1>';
+    echo '<h3 style="color:#6600ff;">Meu nome é: '.$nome.' '.$sobrenome.'</h3>';
+    echo '<p style="color:#6600ff;">Idade: '.$idade. '</p>';
+
+ ?>
