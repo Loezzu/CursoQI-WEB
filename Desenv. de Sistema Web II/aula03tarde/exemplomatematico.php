5a42d549a06fefcdeead23d860246450757b14dd
Luiz Souza <luixsouza12@outlook.com>
2020-11-11 16:54:23 -0300

    create aula03tarde
    Exemplo de criação de classes e funções.
    Exemplo matemático

---
+++ Desenv. de Sistema Web II/aula03tarde/exemplomatematico.php
@@ -0,0 +1,14 @@
+<?php
+
+    // Exemplo para calcular a soma de dois valores
+
+    $valor1 = 10;
+    $valor2 = 5;
+
+    $resultado = $valor1 + $valor2;
+
+    echo '<h3>O resultado da soma é: '.$resultado. '</h3>';
+
+    // http://localhost/aula03tarde/exemplomatematico.php
+ 
+ ?>
---
+++ Desenv. de Sistema Web II/aula03tarde/index.php
@@ -0,0 +1,23 @@
+<?php
+
+ //nome da classe
+  class Pessoa
+  {
+
+ //atributos
+    private $nome;
+    private $idade;
+
+    function calcularIdadeEmMeses()
+    {
+      return $this->idade * 12;
+    }
+  }//fim da classe
+
+
+
+
+
+
+
+ ?>
