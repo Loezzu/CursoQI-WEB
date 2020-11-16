71b86e9f594bff74efceb22e7c9714d587c4b63f
Luiz Souza <luixsouza12@outlook.com>
2020-11-16 04:49:50 -0300

    update aula04tarde_trabalho

--- Desenv. de Sistema Web II/aula04tarde_trabalho/exercicio01/index.php
+++ Desenv. de Sistema Web II/aula04tarde_trabalho/exercicio01/index.php
@@ -6,13 +6,13 @@
     $produto = new Produto();
 
     $produto->setNome("Salgadinho Ruffles");
-    $produto->setValor(3,50);
-    $produto->setQuantidade(10);
+    $produto->setValor(3.50);
+    $produto->setQuantidade(10.00);
 
 
-    echo '<h1>Resultados...:</h1>';
+    echo '<h1>Resultados...</h1>';
 
     echo '<h2>Nome do produto: '.$produto->getNome().'</2>';
-    echo '<h2>Valor final do Produto: R$ '.$produto->calcularValorFinal().'</2>';
+    echo '<h2>Valor final do produto: R$ '.$produto->calcularValorFinal().'</2>';
 
 ?>
