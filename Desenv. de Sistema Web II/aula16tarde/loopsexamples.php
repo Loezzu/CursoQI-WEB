d58da767e307c32998cb277828eb5b3716eeaffa
Luiz Souza <luixsouza12@outlook.com>
2020-12-03 17:03:48 -0300

    create aula16tarde

---
+++ Desenv. de Sistema Web II/aula16tarde/loopsexamples.php
@@ -0,0 +1,20 @@
+<?php
+
+
+    $value = 0;
+
+    while($value <= 20) {
+        if (($value%2) == 0) {
+            echo $value."<br>";
+        }
+        $value++;
+    }
+    
+
+
+
+
+
+
+
+?>
