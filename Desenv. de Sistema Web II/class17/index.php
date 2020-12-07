f24c585e0bd1e7a6afb06a9b5a30f9f7fa6baf0d
Luiz Souza <luixsouza12@outlook.com>
2020-12-07 17:01:25 -0300

    create class17

---
+++ Desenv. de Sistema Web II/class17/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+
+
+    <title>Class 17</title>
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <style type="text/css">
+    h1{
+        text-align: center;
+        color: grey;
+
+    }
+
+    </style>
+
+</head>
+<body>
+
+    <h1>Class 17 - Starting Front - end - Frameworks </h1>
+    
+        
+        <h3>CSS examples:</h3>
+
+        <p style="color:#FF0000; font-size:18pt;"> formatting on the same line </p>
+        <p style="color:blue; font-style:italic;">is the simplest, the elements are applied directly to the markings of the language. However, it does not give us any advantages regarding maintenances.</p>
+
+
+
+            <h4>Embedded CSS</h4>
+
+            <p>They stay with the HTML, but at the beginning of the code. </p>
+
+
+</body>
+</html>
