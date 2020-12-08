260a827b90a72f56df03a2ba49aef6a803fa03d0
Luiz Souza <luixsouza12@outlook.com>
2020-12-08 16:57:25 -0300

    create class18

---
+++ Desenv. de Sistema Web II/class18/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Class 18</title>
+</head>
+<body>
+
+
+    <header>
+    <h1 id="title1">Header</h1>
+    </header>
+
+            <nav class="menu"> 
+                <ul >
+                    <li>Home</li>
+                    <li>Who we are</li>
+                    <li>Services</li>
+                    <li>Affiliates</li>
+                    <li>Contact</li>
+                </ul>
+            
+            </nav>
+
+            <aside>
+                    <h2>QI - Schools and colleges - Visit our website </h2>
+                    <a href="https://qi.edu.br/" target="_blank">
+                    <img src="img/qi.png" alt="qi" witdh="70px" height="70px">
+                
+                    </a>
+
+            </aside>
+
+                <article>
+                        <h2>Unordered list example:</h2>
+                        <ul class="square">
+                        
+                            <li>QI School - Canoas</li>
+                            <li>QI School - Esteio</li>
+                            <li>QI School - Gravataí</li>
+
+                        </ul>
+
+                        <h2>Ordered list example:</h2>
+                        <ol class="letters">
+                        
+                            <li>Canoas RS</li>
+                            <li>Esteio RS</li>
+                            <li>Gravataí RS</li>
+
+                        </ol>
+
+                </article>
+
+                    <footer>
+                    </footer>
+
+
+        
+</body>
+</html>
